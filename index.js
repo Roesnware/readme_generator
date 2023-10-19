d2618ec44d03794259e2ef34637fa09013c0a607
Roesn <Bubwub04@icloud.com>
2023-10-19 14:40:49 -0400

    made questions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inq = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = ["", "", ""];
+const questions = ["What is this app is for?", "How do users use the app?", "How do users install it?", "How can users report issues?", "How can other devs make contributions?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
