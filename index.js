b4a6bd28ffb64419eb3306e4f151220b11d1fe11
Roesn <Bubwub04@icloud.com>
2023-10-19 14:38:21 -0400

    write to file func

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // TODO: Include packages needed for this application
+const inq = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["", "", ""];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        console.log(err)
+    }
+    )
+}
 
 // TODO: Create a function to initialize app
 function init() {}
