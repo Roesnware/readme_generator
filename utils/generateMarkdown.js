65efafa8cabbe9c437f1f513417b675c0b2c3887
Roesn <Bubwub04@icloud.com>
2023-10-19 15:35:54 -0400

    func struc

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { 
-
+    if(!license) {
+        return "";
+    } else {
+        // returns a license badge based on which license is passed in
+    }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) { 
+    if(!license) {
+        return "";
+    } else {
+        // returns the license link
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+    if(!license) {
+        return "";
+    } else {
+        // returns the license section of README
+    }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -41,10 +57,6 @@ function generateMarkdown(data) {
     ## License
     
     The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-    
-    ---
-    
-    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
     
     ## How to Contribute
     
