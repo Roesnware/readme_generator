c3d9b678db99f8cdeff1fb9c5b1fccd4dff32859
Roesn <Bubwub04@icloud.com>
2023-10-19 15:40:05 -0400

    render sec func

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,30 +1,42 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { 
+
+    // check if license null
     if(!license) {
         return "";
     } else {
         // returns a license badge based on which license is passed in
+
     }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) { 
+
+    // check if license null
     if(!license) {
         return "";
     } else {
+
         // returns the license link
+        
     }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+
+    // check if license null
     if(!license) {
         return "";
     } else {
+
         // returns the license section of README
+        renderLicenseLink(license);
+        renderLicenseBadge(license);
     }
  }
 
